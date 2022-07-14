7fc726be5a6556228f2d3c590d8c60250a0e7bd8
nyagah <nyagah@DESKTOP-31HOFM1.localdomain>
2022-07-14 14:04:08 +0300

    Add typeswitcher state and render inputs on select

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -5,20 +5,35 @@ import qs from 'qs'
 import { useNavigate } from 'react-router-dom'
 
 
+
 export default function CreateProduct() {
   const navigate = useNavigate();
 
-  const [inputs, setInputs] = useState(false);
+  const [inputs, setInputs] = useState();
+  const [typeInputs, setTypeInputs] = useState();
   const [errors, setErrors] = useState([])
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState([])
   const handleChange = (e) => {
 
     setInputs({ ...inputs, [e.target.name]: e.target.value });
   }
 
+  const handleTypeChange = (e) => {
+
+    setTypeInputs({ ...typeInputs, [e.target.name]: e.target.value });
+  }
+
+
+  const handleSelect = (e) => {
+    setValue(e.target.value)
+    setTypeInputs({})
+  }
+
+
+  console.log(value);
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:8080/add-product', qs.stringify(inputs))
+    axios.post('http://localhost:8080/add-product', qs.stringify({ ...inputs, ...typeInputs }))
       .then(res => {
         if (res.data.errors) {
           setErrors(res.data.errors)
@@ -33,9 +48,7 @@ export default function CreateProduct() {
 
   }
 
-  const handleSelect = (e) => {
-    setValue(e.target.value)
-  }
+
 
 
   return (
@@ -74,22 +87,26 @@ export default function CreateProduct() {
                   {(errors["price"]) ? <div>{errors["price"][1]}</div> : null}
                 </td>
               </tr>
+
               <tr>
                 <th>
                   <label> Type Switcher: </label>
                 </th>
                 <td>
                   <div >
-                    <select className='form-select' value={optionsState} id="form-selector">
+                    <select className='form-select' id="form-selector" onChange={handleSelect}>
                       <option selected disabled>Type switcher</option>
-                      <option value="dvd">DVD</option>
-                      <option value="book">Book</option>
-                      <option value="dimensions">Furniture</option>
+                      <option value="dvd" >DVD</option>
+                      <option value="book" >Book</option>
+                      <option value="dimensions" >Furniture</option>
                     </select>
                   </div>
+                  {(errors["size"]) ? <div>{errors["size"][0]}</div> : null}
+                  {(errors["weight"]) ? <div>{errors["weight"][0]}</div> : null}
+                  {(errors["height"]) ? <div>{errors["height"][0]}</div> : null}
                 </td>
               </tr>
-              <tr>
+              {(value) === 'dvd' ? <tr>
                 <th>
                   <div id="my-forms">
                     <label> size : </label>
@@ -98,7 +115,7 @@ export default function CreateProduct() {
                 <td>
                   <div id="my-forms">
                     <div id="size">
-                      <input type="number" name="size" onChange={handleChange} />
+                      <input type="number" name="size" onChange={handleTypeChange} />
                       {(errors["size"]) ? <div>{errors["size"][0]}</div> : null}
                       {(errors["size"]) ? <div>{errors["size"][1]}</div> : null}
                       {(errors["size"]) ? <div>{errors["size"][2]}</div> : null}
@@ -106,8 +123,9 @@ export default function CreateProduct() {
                   </div>
 
                 </td>
-              </tr>
-              <tr>
+              </tr> : null}
+
+              {(value) === 'book' ? <tr>
                 <th>
                   <div id="my-forms">
                     <label> weight : </label>
@@ -116,7 +134,7 @@ export default function CreateProduct() {
                 <td>
                   <div id="my-forms">
                     <div id="weight">
-                      <input type="number" name="weight" onChange={handleChange} />
+                      <input type="number" name="weight" onChange={handleTypeChange} />
                       {(errors["weight"]) ? <div>{errors["weight"][0]}</div> : null}
                       {(errors["weight"]) ? <div>{errors["weight"][1]}</div> : null}
                       {(errors["weight"]) ? <div>{errors["weight"][2]}</div> : null}
@@ -124,62 +142,67 @@ export default function CreateProduct() {
                   </div>
 
                 </td>
-              </tr>
+              </tr> : null}
 
-              <tr>
-                <th>
-                  <div id="my-forms">
-                    <label> height : </label>
-                  </div>
-                </th>
-                <td>
-                  <div id="my-forms">
-                    <div id="height">
-                      <input type="number" name="height" onChange={handleChange} />
-                      {(errors["height"]) ? <div>{errors["height"][0]}</div> : null}
-                      {(errors["height"]) ? <div>{errors["height"][1]}</div> : null}
-                      {(errors["height"]) ? <div>{errors["height"][2]}</div> : null}
-                    </div>
-                  </div>
+              {(value) === 'dimensions' ?
+                <>
+                  <tr>
+                    <th>
+                      <div id="my-forms">
+                        <label> height : </label>
+                      </div>
+                    </th>
+                    <td>
+                      <div id="my-forms">
+                        <div id="height">
+                          <input type="number" name="height" onChange={handleChange} />
+                          {(errors["height"]) ? <div>{errors["height"][0]}</div> : null}
+                          {(errors["height"]) ? <div>{errors["height"][1]}</div> : null}
+                          {(errors["height"]) ? <div>{errors["height"][2]}</div> : null}
+                        </div>
+                      </div>
 
-                </td>
-              </tr>
+                    </td>
+                  </tr>
 
-              <tr>
-                <th>
-                  <div id="my-forms">
-                    <label> width : </label>
-                  </div>
-                </th>
-                <td>
-                  <div id="my-forms">
-                    <div id="width">
-                      <input type="number" name="width" onChange={handleChange} />
-                      {(errors["width"]) ? <div>{errors["width"][0]}</div> : null}
-                      {(errors["width"]) ? <div>{errors["width"][1]}</div> : null}
-                    </div>
-                  </div>
+                  <tr>
+                    <th>
+                      <div id="my-forms">
+                        <label> width : </label>
+                      </div>
+                    </th>
+                    <td>
+                      <div id="my-forms">
+                        <div id="width">
+                          <input type="number" name="width" onChange={handleChange} />
+                          {(errors["width"]) ? <div>{errors["width"][0]}</div> : null}
+                          {(errors["width"]) ? <div>{errors["width"][1]}</div> : null}
+                        </div>
+                      </div>
 
-                </td>
-              </tr>
+                    </td>
+                  </tr>
 
-              <tr>
-                <th>
-                  <div id="my-forms">
-                    <label> length : </label>
-                  </div>
-                </th>
-                <td>
-                  <div id="my-forms">
-                    <div id="length">
-                      <input type="number" name="length" onChange={handleChange} />
-                      {(errors["length"]) ? <div>{errors["length"][0]}</div> : null}
-                      {(errors["length"]) ? <div>{errors["length"][1]}</div> : null}
-                    </div>
-                  </div>
+                  <tr>
+                    <th>
+                      <div id="my-forms">
+                        <label> length : </label>
+                      </div>
+                    </th>
+                    <td>
+                      <div id="my-forms">
+                        <div id="length">
+                          <input type="number" name="length" onChange={handleChange} />
+                          {(errors["length"]) ? <div>{errors["length"][0]}</div> : null}
+                          {(errors["length"]) ? <div>{errors["length"][1]}</div> : null}
+                        </div>
+                      </div>
 
-                </td>
-              </tr>
+                    </td>
+                  </tr>
+                </>
+                : null}
+
 
               <tr>
                 <td>
