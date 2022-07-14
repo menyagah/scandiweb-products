eb8c927e94685f3484a9785ca9a59d87cde22241
nyagah <nyagah@DESKTOP-31HOFM1.localdomain>
2022-07-14 14:14:26 +0300

    Import axios, add function to get products using axios and add a useeffect hook

--- src/components/ListProduct.js
+++ src/components/ListProduct.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import AddNav from './Layouts/AddNav';
+import axios from 'axios';
 
 export default function ListProduct() {
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
+    axios.get('http://localhost:8080/products')
+      .then(res => {
+        setProducts(res.data);
+      }).catch(err => {
+        console.log(err);
+      }
+      );
+  }
+
   return (
     <AddNav>
       <div>
