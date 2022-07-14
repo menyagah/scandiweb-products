2b4ae9582d179dbf63083bc815856795016c1d64
nyagah <nyagah@DESKTOP-31HOFM1.localdomain>
2022-07-14 16:52:27 +0300

    Add a map function to display data

--- src/components/ListProduct.js
+++ src/components/ListProduct.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 export default function ListProduct() {
 
-  const [products, setProducts] = useState({});
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     getProducts();
   }, []);
@@ -14,16 +14,33 @@ export default function ListProduct() {
     axios.get('http://localhost:8080/products')
       .then(res => {
         setProducts(res.data);
+        console.log(res.data);
       }).catch(err => {
         console.log(err);
       }
       );
   }
 
+
+
   return (
     <AddNav>
       <div>
         <h1>Product List</h1>
+        {
+          products.map(product => {
+            return (
+              <div key={product.id}>
+                <h2>Sku: {product.sku}</h2>
+                <p>Name: {product.name}</p>
+                <p>Price: {product.price}$</p>
+                <p> {product.size ? <div>Size: {product.size} MB</div> : null}</p>
+                <p> {product.weight ? <div>Weight: {product.weight} KG</div> : null}</p>
+                <p> {product.height || product.width || product.length ? <div>Dimensions: {product.height}x{product.width}x{product.length}</div> : null}</p>
+              </div>
+            )
+          })
+        }
       </div>
     </AddNav>
   )
